4493db32cd2d20ba178df0f5f74ca6651fa58e98
TGeronimo <thi.matheus@gmail.com>
2021-09-23 23:35:20 -0300

    implemented login logic in LoginWindowController and LoginService

--- src/com/baronasu/controller/LoginWindowController.java
+++ src/com/baronasu/controller/LoginWindowController.java
@@ -1,6 +1,8 @@
 package com.baronasu.controller;
 
 import com.baronasu.EmailManager;
+import com.baronasu.controller.services.LoginService;
+import com.baronasu.model.EmailAccount;
 import com.baronasu.view.ViewFactory;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -25,10 +27,35 @@ public class LoginWindowController extends BaseController {
 
     @FXML
     void loginButtonAction() {
+
+        if(fieldsAreValid()) {
+            EmailAccount emailAccount = new EmailAccount(emailAddressField.getText(), passwordField.getText());
+            LoginService loginService = new LoginService(emailAccount, emailManager);
+            EmailLoginResult emailLoginResult = loginService.login();
+
+            switch (emailLoginResult) {
+                case SUCCESS:
+                    System.out.println("login succesfull!!!" + emailAccount);
+            }
+        }
+
         System.out.println("loginButtonAction");
         viewFactory.showMainWindow();
         Stage stage = (Stage) errorLabel.getScene().getWindow();
         viewFactory.closeStage(stage);
+    }
+
+    private boolean fieldsAreValid() {
+        if (emailAddressField.getText().isEmpty()) {
+            errorLabel.setText("Insira o email");
+            return false;
+        }
+
+        if (passwordField.getText().isEmpty()) {
+            errorLabel.setText("Insira a senha");
+            return false;
+        }
+        return true;
     }
 
 }
